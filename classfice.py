10248d83f1b68b30421591c1ace97e707351af00
kanber28 <kanbermustafa10@gmail.com>
2022-01-01 13:09:08 +0300

    First Commit

---
+++ classfice.py
@@ -0,0 +1,117 @@
+import numpy as np
+from tqdm import tqdm
+import librosa
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+audio_dataset_path='UrbanSound8K/audio/'
+metadata=pd.read_csv('UrbanSound8K/metadata/UrbanSound8K.csv')
+metadata.head()
+
+def features_extractor(file):
+    audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
+    mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+    mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
+    
+    return mfccs_scaled_features    
+
+
+### Now we iterate through every audio file and extract features 
+### using Mel-Frequency Cepstral Coefficients
+extracted_features=[]
+for index_num,row in tqdm(metadata.iterrows()):
+    file_name = os.path.join(os.path.abspath(audio_dataset_path),'fold'+str(row["fold"])+'/',str(row["slice_file_name"]))
+    final_class_labels=row["class"]
+    data=features_extractor(file_name)
+    extracted_features.append([data,final_class_labels])
+
+### converting extracted_features to Pandas dataframe
+extracted_features_df=pd.DataFrame(extracted_features,columns=['feature','class'])
+extracted_features_df.head()
+
+### Split the dataset into independent and dependent dataset
+X=np.array(extracted_features_df['feature'].tolist())
+y=np.array(extracted_features_df['class'].tolist())
+
+X.shape
+
+y
+
+### Label Encoding
+###y=np.array(pd.get_dummies(y))
+### Label Encoder
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+labelencoder=LabelEncoder()
+y=to_categorical(labelencoder.fit_transform(y))
+
+### Train Test Split
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+
+X_train
+y
+X_train.shape
+X_test.shape
+y_train.shape
+y_test.shape
+
+
+### MODEL CREATİON
+import tensorflow as tf
+print(tf.__version__)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Dropout,Activation,Flatten
+from tensorflow.keras.optimizers import Adam
+from sklearn import metrics
+
+### No of classes
+num_labels=y.shape[1]
+
+model=Sequential()
+###first layer
+model.add(Dense(100,input_shape=(40,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+###second layer
+model.add(Dense(200))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+###third layer
+model.add(Dense(100))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+###final layer
+model.add(Dense(num_labels))
+model.add(Activation('softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='adam')
+
+## Trianing my model
+from tensorflow.keras.callbacks import ModelCheckpoint
+from datetime import datetime 
+
+num_epochs = 100
+num_batch_size = 32
+
+checkpointer = ModelCheckpoint(filepath='saved_models/audio_classification.hdf5', 
+                               verbose=1, save_best_only=True)
+start = datetime.now()
+
+model.fit(X_train, y_train, batch_size=num_batch_size, epochs=num_epochs, validation_data=(X_test, y_test), callbacks=[checkpointer], verbose=1)
+
+
+duration = datetime.now() - start
+print("Training completed in time: ", duration)
+
+test_accuracy=model.evaluate(X_test,y_test,verbose=0)
+print(test_accuracy[1])
+
+prediction_feature.shape
+
+X_test[1]
+model.predict_classes(X_test)
